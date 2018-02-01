97f8d2418a25ee568f30a0cf5e038b97c457dde6
?ngel ?lvarez <angel@nan-tic.com>
2018-02-01 10:34:09 +0100

    fix setup, add tox and drone file

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .party import *
+from . import party
 
 
 def register():
     Pool.register(
-        Cnae,
-        Party,
+        party.Cnae,
+        party.Party,
         module='party_cnae', type_='model')
